83e3098192ea3d5cbbcb65523e4210082e98655c
JuanDiego34 <juan.diego.34@ull.edu.es>
2023-11-25 22:10:16 +0000

    Intial playlist and artist calls

---
+++ README.md
@@ -0,0 +1,13 @@
+# Spotify notifier
+
+This program lets the user follow certain artists and add tracks to playlists. The user will get notified everytime:
+- A followed artist uploads a song.
+- A track gets added to a playlist.
+
+## Usage
+
+
+
+## More information
+
+This program uses the following Spotify API client: https://github.com/spotify-web-api-java/spotify-web-api-java/tree/master
---
+++ src/main/java/org/example/SpotifyAPIcalls.java
@@ -0,0 +1,66 @@
+package org.example;
+
+import se.michaelthelin.spotify.SpotifyApi;
+import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+import se.michaelthelin.spotify.model_objects.credentials.ClientCredentials;
+import se.michaelthelin.spotify.model_objects.specification.Artist;
+import se.michaelthelin.spotify.model_objects.specification.Playlist;
+import se.michaelthelin.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
+import org.apache.hc.core5.http.ParseException;
+import se.michaelthelin.spotify.requests.data.artists.GetArtistRequest;
+import se.michaelthelin.spotify.requests.data.playlists.GetPlaylistRequest;
+
+import java.io.IOException;
+
+public class SpotifyAPIcalls {
+    // clientID and clientSecret are retrieved from the app created on Spotify Dashboard
+    private static final String clientId = "4dc165c050c6424b9270741eeb871dd0";
+    private static final String clientSecret = "93cc4bc42e344d209fb2c30d4d570413";
+    private static final String idArtist = "3TVXtAsR1Inumwj472S9r4";
+    private static final String idPlaylist = "37i9dQZF1DX0XUsuxWHRQd";
+
+    private static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
+            .setClientId(clientId)
+            .setClientSecret(clientSecret)
+            .build();
+
+    // Set access token for further "spotifyApi" object usage
+    public static void setToken() {
+        try {
+            ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials().build();
+            final ClientCredentials clientCredentials = clientCredentialsRequest.execute();
+
+            spotifyApi.setAccessToken(clientCredentials.getAccessToken());
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void getArtist() {
+        try {
+            GetArtistRequest getArtistRequest = spotifyApi.getArtist(idArtist).build();
+            final Artist artist = getArtistRequest.execute();
+
+            System.out.println("Artist: " + artist.getName());
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void getPlaylist() {
+        try {
+            GetPlaylistRequest getPlaylistRequest = spotifyApi.getPlaylist(idPlaylist).build();
+            final Playlist playlist = getPlaylistRequest.execute();
+
+            System.out.println("Artist: " + playlist.getName());
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        setToken();
+        getArtist();
+        getPlaylist();
+    }
+}
