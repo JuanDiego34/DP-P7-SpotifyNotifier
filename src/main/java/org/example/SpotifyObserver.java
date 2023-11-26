044ae415a8ad42a351642c025ea073c1fa75e619
Eduardo José Barrios García <eduardojbarriosgarcia@gmail.com>
2023-11-26 22:06:57 +0000

    Deleting files, upload branch failure

--- src/main/java/org/example/SpotifyObserver.java
+++
@@ -1,9 +0,0 @@
-package org.example.Observer;
-
-/**
- * Interface for Spotify observers.
- * Declares the update method that gets called when SpotifySubject notifies changes.
- */
-public interface SpotifyObserver {
-    void update();
-}
