8336dbf483929b12308336c2adeaac08e8243200
Eduardo José Barrios García <eduardojbarriosgarcia@gmail.com>
2023-11-27 10:40:33 +0000

    Adding additional functions to start working with the Spotify API

--- src/main/java/org/example/Observer/SpotifySubject.java
+++ src/main/java/org/example/Observer/SpotifySubject.java
@@ -1,13 +1,42 @@
 package org.example.Observer;
-//Commented due to package issue, not needed this import
-//import org.example.Observer.SpotifyObserver;
+
+import se.michaelthelin.spotify.SpotifyApi;
 
 /**
  * Interface for Spotify subjects.
- * This interface declares methods for attaching, detaching, and notifying observers.
+ * Declares methods for attaching, detaching, and notifying observers,
+ * as well as managing interactions with the Spotify API.
  */
 public interface SpotifySubject {
+
+    /**
+     * Attaches an observer to the subject.
+     * 
+     * @param o The observer to be attached.
+     */
     void attach(SpotifyObserver o);
+
+    /**
+     * Detaches an observer from the subject.
+     * 
+     * @param o The observer to be detached.
+     */
     void detach(SpotifyObserver o);
+
+    /**
+     * Notifies all attached observers of a change.
+     */
     void notifyObservers();
+
+    /**
+     * Handles authentication with the Spotify API.
+     */
+    void authenticate();
+
+    /**
+     * Provides access to the Spotify API instance.
+     * 
+     * @return SpotifyApi The instance of SpotifyApi.
+     */
+    SpotifyApi getSpotifyApi();
 }
