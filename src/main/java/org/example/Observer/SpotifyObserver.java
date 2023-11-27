592d10660b12b96a41fbdacf30fdfb5580decfbf
Eduardo José Barrios García <eduardojbarriosgarcia@gmail.com>
2023-11-27 17:01:58 +0000

    Fixing minor issue

--- src/main/java/org/example/Observer/SpotifyObserver.java
+++ src/main/java/org/example/Observer/SpotifyObserver.java
@@ -1,22 +1,9 @@
 package org.example.Observer;
 
 /**
- * Interface for Spotify subjects.
- * This interface declares methods for attaching, detaching, notifying observers,
- * and checking for updates in Spotify data.
+ * Interface for Spotify observers.
+ * Declares the update method that gets called when SpotifySubject notifies changes.
  */
-public interface SpotifySubject {
-
-    /**
-     * Notify all attached observers of a change, when a checkForUpdates method, gets an Update
-     */
-    void notifyObservers();
-
-    /**
-     * Check for updates in Spotify data.
-     * Involve checking for changes in artist/playlist followers, new releases, etc.
-     * In this case the check for updates method, means checking if a verification gap has been marked
-     * This will be implemented at Implementation File and GUI afterwards
-     */
-    void checkForUpdates();
+public interface SpotifyObserver {
+    void update();
 }
