2829753575de61814336635ff906cc8f898aa2e0
Eduardo José Barrios García <eduardojbarriosgarcia@gmail.com>
2023-11-27 11:04:16 +0000

    Creating Playlist Package

---
+++ src/main/java/org/example/Observer/Playlists/PlaylistSubjectImpl.java
@@ -0,0 +1,9 @@
+package org.example.Observer.Playlists;
+
+import org.example.Observer.SpotifySubjectImpl;
+
+/**
+ * Class specialized in handling Spotify playlists.
+ */
+public class PlaylistSubjectImpl extends SpotifySubjectImpl {
+}
