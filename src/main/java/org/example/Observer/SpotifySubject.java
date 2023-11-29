5cd24d46f27f837e102f18313b93a451220887ad
Eduardo José Barrios García <eduardojbarriosgarcia@gmail.com>
2023-11-29 08:48:29 +0000

    Fixing file structure to adpat it to work with GUI

--- src/main/java/org/example/Observer/SpotifySubject.java
+++ src/main/java/org/example/Observer/SpotifySubject.java
@@ -1,42 +1,63 @@
 package org.example.Observer;
 
 import se.michaelthelin.spotify.SpotifyApi;
+import se.michaelthelin.spotify.model_objects.specification.Image;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Interface for Spotify subjects.
- * Declares methods for attaching, detaching, and notifying observers,
- * as well as managing interactions with the Spotify API.
+ * @brief Abstract class representing a Spotify subject.
+ * 
+ * This class serves as an abstract base for subjects in the observer pattern,
+ * specifically for Spotify-related functionality. It provides methods for 
+ * attaching, detaching, and notifying observers, as well as managing 
+ * interactions with the Spotify API.
  */
-public interface SpotifySubject {
+public abstract class SpotifySubject {
+    
+    protected final List<SpotifyObserver> observers; /**< List of observers to notify about changes. */
+    protected final SpotifyApi spotifyApi; /**< Instance of SpotifyApi for interacting with the Spotify API. */
 
     /**
-     * Attaches an observer to the subject.
+     * Constructor for SpotifySubject.
+     * Initializes the list of observers and sets up the Spotify API connection.
+     */
+    public SpotifySubject() {
+        observers = new ArrayList<>();
+        SpotifyApiManager spotifyApiManager = new SpotifyApiManager();
+        spotifyApiManager.authenticate();
+        this.spotifyApi = spotifyApiManager.getSpotifyApi();
+    }
+
+    /**
+     * @brief Attaches an observer to the subject.
      * 
-     * @param o The observer to be attached.
+     * @param o The SpotifyObserver to be attached.
      */
-    void attach(SpotifyObserver o);
+    public void attach(SpotifyObserver o) {
+        observers.add(o);
+    }
 
     /**
-     * Detaches an observer from the subject.
+     * @brief Detaches an observer from the subject.
      * 
-     * @param o The observer to be detached.
+     * @param o The SpotifyObserver to be detached.
      */
-    void detach(SpotifyObserver o);
+    public void detach(SpotifyObserver o) {
+        observers.remove(o);
+    }
 
     /**
-     * Notifies all attached observers of a change.
-     */
-    void notifyObservers();
-
-    /**
-     * Handles authentication with the Spotify API.
-     */
-    void authenticate();
-
-    /**
-     * Provides access to the Spotify API instance.
+     * @brief Abstract method to get the name of the subject.
      * 
-     * @return SpotifyApi The instance of SpotifyApi.
+     * @return String representing the name of the subject.
      */
-    SpotifyApi getSpotifyApi();
+    public abstract String getName();
+
+    /**
+     * @brief Abstract method to get the cover image of the subject.
+     * 
+     * @return Image object representing the cover of the subject.
+     */
+    public abstract Image getCover();
 }
