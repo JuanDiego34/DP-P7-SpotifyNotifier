a107fa7195c74d3d1dbee75ccfe4ebb5730e6ff0
Petrov Oleh <81036385+petrovoleh@users.noreply.github.com>
2023-11-30 17:38:27 +0000

    added notifications

---
+++ src/main/java/org/example/Observer/Notification.java
@@ -0,0 +1,44 @@
+package org.example.Observer;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Notification {
+    public static void showNotification(String message) {
+        // Create a JFrame for the notification
+        JFrame notificationFrame = new JFrame("Notification");
+        notificationFrame.setSize(300, 100);
+        notificationFrame.setUndecorated(true);
+
+        // Set the location to be more to the right of the screen
+        int screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
+        int screenHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
+        int xPosition = screenWidth / 2 - notificationFrame.getWidth() / 4; // Adjust the value as needed
+        int yPosition = screenHeight / 2;
+
+        notificationFrame.setLocation(xPosition, yPosition);
+
+        // Create a JLabel to display the message
+        JLabel messageLabel = new JLabel(message);
+        messageLabel.setHorizontalAlignment(JLabel.CENTER);
+        notificationFrame.add(messageLabel);
+
+        // Set a timer to close the notification after a certain duration (e.g., 3 seconds)
+        Timer timer = new Timer(3000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                notificationFrame.dispose();
+            }
+        });
+        timer.setRepeats(false); // Set to false to run only once
+        timer.start();
+
+        // Set the background color and make it slightly transparent
+        notificationFrame.getContentPane().setBackground(new Color(220, 220, 220, 200));
+
+        // Show the notification
+        notificationFrame.setVisible(true);
+    }
+}
